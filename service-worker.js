468237de43a52df0e29dada11af4acd6ee0b71e9
dev-jonghoonpark <dev@jonghoonpark.com>
2023-10-01 16:44:06 +0900

    apply pwa

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+// service-worker.js
+
+// set names for both precache & runtime cache
+workbox.core.setCacheNameDetails({
+  prefix: 'dev-jonghoonpark',
+  suffix: 'v1.0',
+  precache: 'precache',
+  runtime: 'runtime-cache'
+});
+
+// let Service Worker take control of pages ASAP
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+// let Workbox handle our precache list
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// use `NetworkFirst` strategy for html
+workbox.routing.registerRoute(
+  /\.html$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+// use `NetworkFirst` strategy for css and js
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+// use `CacheFirst` strategy for images
+workbox.routing.registerRoute(
+  /assets\/(images|icons)/,
+  new workbox.strategies.CacheFirst()
+);
