3aded4e2d89ac05dc21bfd4d21e5a9fe9e736331
dev-jonghoonpark <dev@jonghoonpark.com>
2023-10-02 21:17:09 +0900

    change suffix of workbox

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
   prefix: 'dev-jonghoonpark',
-  suffix: 'v1.0',
+  suffix: 'v1.1',
   precache: 'precache',
   runtime: 'runtime-cache'
 });
