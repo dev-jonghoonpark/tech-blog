450c007640370f3d54bbaf3dccbb962fa2756197
dev-jonghoonpark <dev@jonghoonpark.com>
2023-10-09 22:19:40 +0900

    이전 글들 html 형태로 1차 migration

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
   prefix: 'dev-jonghoonpark',
-  suffix: 'v1.1',
+  suffix: 'v1.2',
   precache: 'precache',
   runtime: 'runtime-cache'
 });
